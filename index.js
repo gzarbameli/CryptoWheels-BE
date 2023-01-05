2a1b55e4848e88cae5105cc71563a71eb3bf5278
Giacomo Zarba Meli <giacomo.zarba@immobiliare.it>
2023-01-05 11:53:31 +0100

    comments refactor

--- index.js
+++ index.js
@@ -10,17 +10,18 @@ const fs = require('fs');
 app.use(express.json())
 app.use(cors());
 
+// Define the endpoint create-composite
 app.post('/create-composite', async (req, res) => {
     
-  // Legge l'array di CID dal corpo della richiesta
+  // Read the CID Array from the request body 
   let cidArray = req.body;
 
-  // In cima all'array aggiungi l'image CID della macchina stock
-  cidArray.unshift('QmWLoMS4sAFngUJoCfGif211MvGGR6kG9XaafzyWoJXgQU');
+  // Insert at the beginning of the CID array the CID of the stock car image
+  cidArray.unshift('QmVz6CoMLu6iFy87T1fmHRPbX5iF3zuWMetD7DLMAAamWm');
 
   console.log(cidArray);
 
-  // Carica le immagini dall'array di CID, componendo le URL
+  // Load the images from the CID array by creating the respective URLs 
   let images = await Promise.all(cidArray.map(async cid => {
     let imageUrl = `https://crypto-wheels.infura-ipfs.io/ipfs/${cid}`;
     let imageResponse = await axios({url: imageUrl, responseType: 'arraybuffer'})
@@ -28,33 +29,34 @@ app.post('/create-composite', async (req, res) => {
     return imageBuffer;
   }));
 
-
-  // Crea un nuovo canvas con le dimensioni desiderate
+  // Create a new canvas with the desired options
   let canvas = Sharp({
     create: {
-      width: 800,
-      height: 600,
+      width: 1000,
+      height: 1000,
       channels: 4,
       background: { r: 255, g: 255, b: 255 }
     }
   });
 
+  // Create the layers to be applied on the canvas
   let layers = images.map(file => ({ input: file }));
 
+  // Composite the layers on the original canvas
   canvas = canvas.composite(layers); 
 
-  // Restituisce l'immagine come risposta alla richiesta
-  let compositeImage = await canvas.toFile('merged.jpg');
+  // Save the composed image in a jpg file (overwritten everytime)
+  await canvas.toFile('merged.jpg');
 
-  // Prendi il file mergiato e fai encoding a base64 per passarlo a react in response
+  // Load the merged jpg file and encode it to base64 format
   const fileContent = fs.readFileSync('./merged.jpg');
   const base64 = btoa(new Uint8Array(fileContent).reduce((data, byte) => data + String.fromCharCode(byte), ''));
 
+  // Send as response the base64 encoded image
   res.send(base64);
-
 });
 
-// Avvia il server
+// Start the server
 app.listen(port, () => {
   console.log(`Backend for Image Composition listening on port: ${port}`);
 });
